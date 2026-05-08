c720ad3c9a220f206c1448ebd68d08028726c5ba
aacho04 <aaditinarayan14@gmail.com>
2026-05-08 19:17:57 +0530

    Improve layout components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
-import Navbar from "./components/navbar/Navbar"
-import './layout.scss'
+import Navbar from "./components/navbar/Navbar.jsx";
+import HomePage from "./routes/HomePage.jsx";
+import "./layout.scss";
 
 
 function App() {
 
   return (
    <div className="layout">
-    <Navbar/>
-   </div>
+    <div className="navbar"><Navbar/></div>
+    <div className="content"><HomePage/></div>
+  </div>
   )
 }
 
