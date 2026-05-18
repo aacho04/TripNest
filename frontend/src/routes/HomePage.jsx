9a5466bcc9a047b70bae2e6b18819898138d9831
aacho04 <aaditinarayan14@gmail.com>
2026-05-18 12:47:35 +0530

    style: improve homepage layout and responsive sections

--- frontend/src/routes/HomePage.jsx
+++ frontend/src/routes/HomePage.jsx
@@ -1,33 +1,36 @@
-import '../components/navBar/Navbar.jsx';
-import '../components/searchBar/SearchBar.jsx'; 
+import SearchBar from '../components/searchbar/SearchBar.jsx'; 
 import '../layout.scss';
-import './homePage.scss';
+import './homepage.scss';
 function HomePage(){
     return(
         <div className="homePage">
             <div className="textContainer">
                 <div className="wrapper">
-                    <div className="title">
-                        <h1>Find, Book, and Belong — Your Perfect Space Awaits</h1>
-                        <p>--
-                            Discover your ideal workspace with our platform. Whether you're a freelancer, entrepreneur, or remote worker, we connect you with the perfect space to boost your productivity and creativity. Book your next workspace today and experience the freedom to work where you thrive.
+                    <h1 className="title">
+                        Find, Book, and Belong — Your Perfect Space Awaits</h1>
+                        <p>— 
+                            Discover your ideal workspace with our platform. Book your next workspace today and experience the freedom to work where you thrive.
                         </p>
                         <SearchBar/>
                         <div className="boxes">
-                            <h1>15+</h1>
-                            <h2>Years of Experience</h2>
+                            <div className="box">
+                                 <h1>15+</h1>
+                                 <h2>Years of Experience</h2>
+                            </div>
+                            <div className="box">
+                                <h1>200</h1>
+                                <h2>Award Gained</h2>
+                            </div>
+                            <div className="box">
+                                    <h1>1200+</h1>
+                                    <h2>Property Ready</h2>
+                            </div>
+                           
                         </div>
-                        <div className="boxes">
-                            <h1>200</h1>
-                            <h2>Award Gained</h2>
-                        </div>
-                        <div className="boxes">
-                            <h1>1200+</h1>
-                            <h2>Property Ready</h2>
-                        </div>
+                        
                     </div>
                      
-                </div>
+                
                
 
             </div>
