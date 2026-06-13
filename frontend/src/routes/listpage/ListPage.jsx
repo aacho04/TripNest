0bb82147097787ab33cf1a083b22e3c639b51151
aacho04 <aaditinarayan14@gmail.com>
2026-06-13 10:43:22 +0530

    Formatted responsive layout structure to avoid styling alignment conflicts

--- frontend/src/routes/listpage/ListPage.jsx
+++ frontend/src/routes/listpage/ListPage.jsx
@@ -1,10 +1,24 @@
 import './listpage.scss';
-import {listData} from '../../lib/dummydata'
+import {listData} from '../../lib/dummydata';
+import Filter from '../../components/filter/Filter';
+import Card from '../../components/card/Card';
+import Map from '../../components/map/Map';
+
 function ListPage(){
     const data=listData;
     return(
         <div className="listPage">
-            <h1>List Page</h1>
+            <div className="listContainer">
+                <div className="wrapper">
+                    <Filter/>
+                    {data.map(item=>(
+                        <Card key={item.id} item={item}/>
+                    ))}
+                </div>
+            </div>
+            <div className="mapContainer">
+                <Map items={data}/>
+            </div>
         </div>
     );
 }
