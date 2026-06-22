48039d7e54a0107d3789b8e467f9f9a8df274c10
aacho04 <aaditinarayan14@gmail.com>
2026-06-22 16:40:20 +0530

    Added features (right side components)

--- frontend/src/routes/singlepage/SinglePage.jsx
+++ frontend/src/routes/singlepage/SinglePage.jsx
@@ -1,6 +1,7 @@
 import './singlepage.scss';
 import Slider from '../../components/slider/Slider';
 import {singlePostData,userData} from "../../lib/dummydata";
+import Map from"../../components/map/Map";
 
 function SinglePage(){
     return(
@@ -30,7 +31,88 @@ function SinglePage(){
                 </div>
             </div>
             <div className="features">
-                <div className="wrapper"></div>
+                <div className="wrapper">
+                        <p className="title">General</p>
+                       <div className="listVertical">
+                        <div className="feature">
+                            <img src="./utility.png" alt="" />
+                            <div className="featureText">
+                                <span>Utility</span>
+                                <p>Renter is responsible</p>
+                            </div>
+                        </div>
+                        <div className="feature">
+                            <img src="./pet.png" alt="" />
+                            <div className="featureText">
+                                <span>Pet Policy</span>
+                                <p>Pet Allowed</p>
+                            </div>
+                        </div>
+                        <div className="feature">
+                            <img src="./fee.png" alt="" />
+                            <div className="featureText">
+                                <span>Property Fees</span>
+                                <p>Must have 3x the rent in total household income</p>
+                            </div>
+                        </div>
+                </div> 
+                       
+                        <p className="title">Room Sizes</p>
+                        <div className="sizes">
+                            <div className="size">
+                                <img src="./size.png" alt="" />
+                                <span>80 sq ft</span>
+                            </div>
+                            <div className="size">
+                                <img src="./bed.png" alt="" />
+                                <span>2 Beds</span>
+                            </div>
+                            <div className="size">
+                                <img src="./bath.png" alt="" />
+                                <span>1 Bathroom</span>
+                            </div>
+                        </div>
+
+                        <p className="title">Nearby Places</p>
+                        <div className="listHorizontal">
+                            <div className="feature">
+                            <img src="./school.png" alt="" />
+                            <div className="featureText">
+                                <span>School</span>
+                                <p>250m</p>
+                            </div>
+                        </div>
+                        <div className="feature">
+                            <img src="./bus.png" alt="" />
+                            <div className="featureText">
+                                <span>Bus Stop</span>
+                                <p>500m away</p>
+                            </div>
+                        </div>
+                        <div className="feature">
+                            <img src="./restaurant.png" alt="" />
+                            <div className="featureText">
+                                <span>Restaurant</span>
+                                <p>300m away</p>
+                            </div>
+                        </div>
+                        </div>
+
+                        <p className="title">Location</p>
+                        <div className="mapContainer">
+                            <Map items={[singlePostData]} />
+                        </div>
+                        <div className="buttons">
+                            <button>
+                                <img src="./chat.png" alt="" />
+                                Send a Message
+                            </button>
+                            <button>
+                                <img src="./save.png" alt="" />
+                               Save the Place
+                            </button>
+                        </div>
+                </div>
             </div>
         </div>
     )
