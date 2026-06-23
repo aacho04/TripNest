a069cea1ac2204f040e59bd0869076dedd5b5fee
aacho04 <aaditinarayan14@gmail.com>
2026-06-23 18:19:24 +0530

    feat: add user profile page with listings and chat section

---
+++ frontend/src/routes/profilepage/ProfilePage.jsx
@@ -0,0 +1,40 @@
+import List from '../../components/list/List';
+import './profilepage.scss';
+import Chat from '../../components/chat/Chat';
+
+function ProfilePage() {
+
+    return(
+        <div className="profilepage">
+            <div className="details">
+                <div className="wrapper">
+                    <div className="title">
+                        <h1>User Information</h1>
+                        <button>Update Profile</button>
+                    </div>
+                    <div className="info">
+                        <span>Avatar:<img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="User Avatar"/></span>
+                        <span>Username: <b>John Doe</b></span>
+                        <span>Email: <b>john@gmail.com</b></span>
+                    </div>
+                    <div className="title">
+                        <h1>My List</h1>
+                        <button>Create New Profile</button>
+                    </div>
+                    <List/>
+                    <div className="title">
+                        <h1>Saved List</h1>
+                        
+                    </div>
+                    <List/>
+                </div>
+            </div>
+            <div className="chatContainer">
+                <div className="wrapper">
+                <Chat/>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default ProfilePage;
